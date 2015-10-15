a3f897a754b03088f7c9b46ce35fe8de1922a24d
Johannes Mey <johannes.mey@tu-dresden.de>
2015-10-15 20:21:14 +0200

    improvements

--- release.sh
+++ release.sh
@@ -10,6 +10,9 @@ echo " Festlegen der Version, welche erstellt werden soll                      "
 echo "========================================================================="
 echo
 
+read -rsp $'Press any key to continue... (or Ctrl-C to exit)\n' -n1 key
+
+
 # grep for the version in source\tudscr-version.dtx
 # 
 VERSION=`egrep -o "\\@TUDVersion{([0-9]{4}/[0-9][0-9]/[0-9][0-9])" source/tudscr-version.dtx | egrep -o [0-9].*`
@@ -31,6 +34,9 @@ echo " Erzeugen der Klassen und der inline-Dokumentation fuer $VERSION"
 echo "========================================================================="
 echo
 
+read -rsp $'Press any key to continue... (or Ctrl-C to exit)\n' -n1 key
+
+
 ./update_classes.sh > log.log
 cp -r source temp
 cd temp
@@ -45,12 +51,12 @@ mkdir -p doc/latex/tudscr/tutorials
 echo '\BaseDirectory{.}' > docstrip.cfg
 echo '\UseTDS'          >> docstrip.cfg
 tex tudscr.ins > log.log
-pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}' > log.log
-pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}' > log.log
-makeindex -s gglo.ist -o tudscrsource.gls tudscrsource.glo > log.log
-makeindex -s gind.ist -o tudscrsource.ind tudscrsource.idx > log.log
-pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}' > log.log
-pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}' > log.log
+pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}'
+makeindex -s gglo.ist -o tudscrsource.gls tudscrsource.glo
+makeindex -s gind.ist -o tudscrsource.ind tudscrsource.idx
+pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscrsource.tex}'
 mv    *.dtx            source/latex/tudscr/
 mv    tudscr.ins       source/latex/tudscr/
 mv    tudscrsource.tex source/latex/tudscr/
@@ -64,105 +70,121 @@ mv    tudscrsource.pdf doc/latex/tudscr/
 mv    logo             tex/latex/tudscr/
 rm *?.?* 
 
-# echo
-# echo "========================================================================="
-# echo " Erzeugen des Benutzerhandbuchs"
-# echo "========================================================================="
-# echo
-# 
-# cd doc
-# pdflatex -shell-escape '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex -shell-escape '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# pdflatex '\def\tudfinalflag{}\def\tudprintflag{}\input{tudscr.tex}' > log.log
-# cp tudscr.pdf tudscr_print.pdf
-# pdflatex '\def\tudfinalflag{}\input{tudscr.tex}' > log.log
-# rm tutorials\*autopp*.*
-# 
-# find tutorials/*.pdf | grep -v '\-temp.pdf' | grep -v '\-pics.pdf' | xargs -I X mv X latex\tudscr
-# 
-# for i in tutorials/*-temp.pdf
-# do
-#   j=`basename $i`
-#   cp $i ../source/latex/tudscr/doc/examples/${j/-temp}
-# done
-# 
-# rm *?.?*
-# rm -r examples
-# rm -r tutorials
-# cd ..
+echo
+echo "========================================================================="
+echo " Erzeugen des Benutzerhandbuchs"
+echo "========================================================================="
+echo
 
-# echo
-# echo "========================================================================="
-# echo "Erzeugen der Installationdateien"
-# echo "========================================================================="
-# echo
-# 
-# cd install
-# cp ../source/latex/tudscr/tudscr-version.dtx ..
-# tex tud-install.ins
-# tex tudscr-install.ins
-# for i in *.bxt
-# do
-#   mv $i ${i/.bxt/.bat}
-# done
-# 
-# for i in *_VER_*
-# do
-#   mv $i ${i/_VER_/_${VERSION}_}
-# done
-# 
-# rm ../tudscr-version.dtx
+read -rsp $'Press any key to continue... (or Ctrl-C to exit)\n' -n1 key
 
-# echo
-# echo "========================================================================="
-# echo " Release fuer GitHub"
-# echo "========================================================================="
-# echo
-# 
-# cd $MYDIR
-# mkdir release-$VERSION/temp
-# mkdir release-$VERSION/GitHub
-# cp *.md                                   release-$VERSION/temp/
-# cp addon                                  release-$VERSION/temp/
-# cp development/fonts/*.*                  release-$VERSION/temp/
-# cp development/tools/*.*                  release-$VERSION/temp/
-# cp temp/doc/latex/tudscr/tudscr.pdf       release-$VERSION/temp/
-# cp temp/doc/latex/tudscr/tudscr_print.pdf release-$VERSION/temp/
-# mv temp\install\*.*                       release-$VERSION/temp/
-# rm -r temp/install
-# cd release-$VERSION/temp
-# for /f %%f in ('dir /b *.bat') do unix2dos -k %%f
-# 7za a -tzip tudscr_%version%_full.zip   .\..\..\temp\*
-# 7za a -tzip tudscr_%version%_update.zip .\..\..\temp\* -xr!logo
-# 7za a -tzip tudscr_fonts_install.zip                                              @7za_files_metrics.txt
+
+cd doc
+pdflatex -shell-escape '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex -shell-escape '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex '\def\tudfinalflag{}\input{tudscr.tex}'
+pdflatex '\def\tudfinalflag{}\def\tudprintflag{}\input{tudscr.tex}'
+cp tudscr.pdf tudscr_print.pdf
+pdflatex '\def\tudfinalflag{}\input{tudscr.tex}'
+rm tutorials\*autopp*.*
+
+find tutorials/*.pdf | grep -v '\-temp.pdf' | grep -v '\-pics.pdf' | xargs -I X mv X latex/tudscr
+
+for i in tutorials/*-temp.pdf
+do
+  j=`basename $i`
+  cp $i ../source/latex/tudscr/doc/examples/${j/-temp}
+done
+
+cp tudscr*.pdf latex/tudscr
+
+rm *?.?*
+rm -r examples
+rm -r tutorials
+cd ..
+
+echo
+echo "========================================================================="
+echo "Erzeugen der Installationdateien"
+echo "========================================================================="
+echo
+
+read -rsp $'Press any key to continue... (or Ctrl-C to exit)\n' -n1 key
+
+
+cd install
+cp ../source/latex/tudscr/tudscr-version.dtx ..
+tex tud-install.ins
+tex tudscr-install.ins
+for i in *.bxt
+do
+  mv $i ${i/.bxt/.bat}
+done
+
+for i in *_VER_*
+do
+  mv $i ${i/_VER_/_${VERSION}_}
+done
+
+rm ../tudscr-version.dtx
+
+echo
+echo "========================================================================="
+echo " Release fuer GitHub"
+echo "========================================================================="
+echo
+
+read -rsp $'Press any key to continue... (or Ctrl-C to exit)\n' -n1 key
+
+
+cd $MYDIR
+mkdir -p release-$VERSION/temp
+mkdir -p release-$VERSION/GitHub
+cp *.md                                   release-$VERSION/temp/
+cp -r addon/*                             release-$VERSION/temp/
+cp development/fonts/*.*                  release-$VERSION/temp/
+cp development/tools/*.*                  release-$VERSION/temp/
+cp temp/doc/latex/tudscr/tudscr.pdf       release-$VERSION/temp/
+cp temp/doc/latex/tudscr/tudscr_print.pdf release-$VERSION/temp/
+mv temp/install/*.*                       release-$VERSION/temp/
+rm -r temp/install
+cd release-$VERSION/temp
+for f in *.bat
+do
+  unix2dos -k $f
+done
+7za a -tzip tudscr_${VERSION}_full.zip   ./../../temp/*
+7za a -tzip tudscr_${VERSION}_update.zip ./../../temp/* -xr!logo
+7za a -tzip tudscr_fonts_install.zip
 # call tudscr_fonts_convert.bat
-# 7za a -tzip tudscr_fonts_converted.zip                                            @7za_files_fonts.txt
-# for /f %%f in ('dir /b *.md') do unix2dos -n -k %%f %%~nf.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Windows_all.zip    -x!*.sh             @7za_files_full.txt @7za_files_postscript.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Windows_full.zip   -x!*.sh             @7za_files_full.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Windows_update.zip -x!*.sh             @7za_files_update.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_fonts_Windows.zip            -x!*.sh             @7za_files_fonts_install.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_fonts_converted_Windows.zip  -x!*.sh             @7za_files_fonts_converted.txt
-# for /f %%f in ('dir /b *.md') do copy %%f %%~nf.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Unix_all.zip       -x!*.bat -x!7za.exe @7za_files_full.txt @7za_files_postscript.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Unix_full.zip      -x!*.bat -x!7za.exe @7za_files_full.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_%version%_Unix_update.zip    -x!*.bat -x!7za.exe @7za_files_update.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_fonts_Unix.zip               -x!*.bat -x!7za.exe @7za_files_fonts_install.txt
-# 7za a -tzip .\..\TUD-KOMA-Script_fonts_converted_Unix.zip     -x!*.bat -x!7za.exe @7za_files_fonts_converted.txt
-# 7za a -tzip .\..\tudscr4lyx.zip       .\tudscr4lyx\*
-# 7za a -tzip .\..\tudscr4texstudio.zip .\tudscr4texstudio\*
-# cd..
-# attrib +h *_all.zip
-# attrib +h *_converted*.zip
-# move *.zip GitHub\
-# attrib -h *_converted*.zip
-# attrib -h *_all.zip
-# move temp\tudscr_%version%_full.zip  GitHub\tudscr_%version%.zip
-# copy temp\tudscr_uninstall.*         GitHub\
+7za a -tzip tudscr_fonts_converted.zip
+for f in *.md
+do
+  unix2dos -k -n $f ${f/.md/.txt}
+done
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Windows_all.zip    -x!*.sh
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Windows_full.zip   -x!*.sh
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Windows_update.zip -x!*.sh
+7za a -tzip ./../TUD-KOMA-Script_fonts_Windows.zip             -x!*.sh
+7za a -tzip ./../TUD-KOMA-Script_fonts_converted_Windows.zip   -x!*.sh
+for f in *.md
+do
+  cp $f ${f/.md/.txt}
+done
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Unix_all.zip       -x!*.bat -x!7za.exe
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Unix_full.zip      -x!*.bat -x!7za.exe
+7za a -tzip ./../TUD-KOMA-Script_${VERSION}_Unix_update.zip    -x!*.bat -x!7za.exe
+7za a -tzip ./../TUD-KOMA-Script_fonts_Unix.zip                -x!*.bat -x!7za.exe
+7za a -tzip ./../TUD-KOMA-Script_fonts_converted_Unix.zip      -x!*.bat -x!7za.exe
+7za a -tzip ./../tudscr4lyx.zip       ./tudscr4lyx/*
+7za a -tzip ./../tudscr4texstudio.zip ./tudscr4texstudio/*
+cd ..
+ls *.zip | grep -v "*_converted*.zip" | grep -v "_all.zip" | xargs -I X mv X GitHub/
+mv temp/tudscr_${VERSION}_full.zip  GitHub/tudscr_${VERSION}.zip
+cp temp/tudscr_uninstall.*        GitHub/
 
 # echo
 # echo =========================================================================
